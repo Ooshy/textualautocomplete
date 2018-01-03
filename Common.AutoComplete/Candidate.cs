959cc78ff48d0a2d33eb130c335d3a1f21ca80d4
Chris Zimmerman <chrisxzimmerman@gmail.com>
2018-01-02 19:47:42 -0500

    documentation and added exception thrown when confidence is below 0

--- Common.AutoComplete/Candidate.cs
+++ Common.AutoComplete/Candidate.cs
@@ -1,20 +1,29 @@
 ﻿using Common.IAutoComplete;
 using System;
-//using TCandidates = System.Collections.Generic.IEnumerable<TextCollections.ITrie.ITrieNode<string>>;
 
 namespace TextualAutocomplete
 {
     public class Candidate : ICandidate
     {
+        /// <summary>
+        /// A word within a given text.
+        /// </summary>
         public string Word { get; }
 
+        /// <summary>
+        /// The likelihood that the word occurs within a given text. Useful when compared against other Candidate's confidence values.
+        /// </summary>
         public int Confidence { get; }
 
+        /// <summary>
+        /// A word and its likelihood to appear in a given text.
+        /// </summary>
         public Candidate(string word, int confidence)
         {
             if ((Word = word) == null)
                 throw new ArgumentNullException($"{nameof(word)} is null.");
-
+            if (confidence < 0)
+                throw new InvalidOperationException($"{nameof(confidence)} is below 0.");
             Confidence = confidence; // value type, so no null check
         }
     }
